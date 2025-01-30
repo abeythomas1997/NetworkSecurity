168f491d47d16a06a7025fafcec212d6ceb089b5
abeythomas1997 <abeythomas990@gmail.com>
2025-01-30 23:50:33 +1100

    dataIngestion_updates

---
+++ NetworkSecurity/Components/data_ingestion.py
@@ -0,0 +1,25 @@
+from NetworkSecurity.Exception.exception import NetworkSecurityException
+from NetworkSecurity.Logging.logger import logging
+
+## configuration of the Data Ingestion Config
+from NetworkSecurity.Entity.config_entity import DataIngestionConfig
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+import pymongo
+from typing import List
+from sklearn.model_selection import train_test_split
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+
+class DataIngestion:
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
+        try:
+            self.data_ingestion_config=data_ingestion_config
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
