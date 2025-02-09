783ac6ab0d31c3a3f83c2fcd2b717d907b86f2a6
abeythomas1997 <abeythomas990@gmail.com>
2025-02-09 12:23:33 +1100

    transformation updated

--- NetworkSecurity/Entity/artifacts_entity.py
+++ NetworkSecurity/Entity/artifacts_entity.py
@@ -15,5 +15,11 @@ class DataValidationArtifact:
     invalid_test_file_path : str
     drfit_report_file_path : str
 
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+
 
 
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from NetworkSecurity.Components.data_ingestion import DataIngestion
 from NetworkSecurity.Exception.exception import NetworkSecurityException
 from NetworkSecurity.Logging.logger import logging
-from NetworkSecurity.Entity.config_entity import DataIngestionConfig,DataValidationConfig
+from NetworkSecurity.Entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from NetworkSecurity.Entity.config_entity import TrainingPipelineConfig
 from NetworkSecurity.Components.data_validation import DataValidation
-
+from NetworkSecurity.Components.data_transformation import DataTransformation
  
 
 import sys
@@ -23,9 +23,12 @@ if __name__=='__main__':
         logging.info("Initiate the data validation")
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
-        
-        
-        
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        logging.info("Initiate the data transformation")
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info("data Transformation Completed")
+
         
     except Exception as e:
            raise NetworkSecurityException(e,sys)
